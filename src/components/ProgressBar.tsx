a7fb1715df47d04964711cabe47bb69ce6a17306
Konstantin <yakimtsev.konstantin@gmail.com>
2025-06-08 15:55:59 +0500

    ver 1.4

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "../styles/progressBar.module.css";
+import styles from "../styles/progresBar.module.css";
 
 interface ProgressBarProps {
     currentScreen: string;
